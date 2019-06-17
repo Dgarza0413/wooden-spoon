10873f47ac2fbf3e6fb20937b42ce9ae9807a3c3
David Garza <dgarza0413@gmail.com>
2019-06-16 22:40:57 -0500

    added ajax to generate a limited number of recipes

--- assets/javascript/ajax.js
+++ assets/javascript/ajax.js
@@ -5,7 +5,7 @@ console.log("loaded ajax file")
 //i will match 
 
 // var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/quickAnswer"
-var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/479101/information"
+// var queryURL = " https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/random?number=1"
 var apiKey = "46a4bca137msh8d35c096a6b18f1p114dfdjsn060845846032"
 var limits = ''
 var types = ''
@@ -19,22 +19,53 @@ $.ajax({
     }
 }).then(function (response) {
     console.log(response)
-    console.log(response.title)
-    console.log(response.servings)
-    console.log(response.instructions)
-    console.log(response.image)
-    // id will be used to call this particular recipe item
-    console.log(response.id)
+    console.log(response.recipes)
+    console.log(response.recipes[0].title)
 
     // function generateRecipes (){}
     // this function has to be used to generate the recipes on the page
-    for (var i = 0; i < response.length; i++) {
-        console.log(response)
-        $("#title-card").text(response.title)
-        $("#servings-card").text(response.servings)
-        $("#prep-time-card").text(response.cookingMinutes)
-        $("#diet-type-card").text(response.diets[0])
-        $("#recipe-img-card").text(response.images)
+    for (var i = 0; i < response.recipes.length; i++) {
+        console.log(response.recipes[i])
+        var card = $("<div>")
+        var cardBody = $("<div>")
+        var cardTitle = $("<div>")
+        var cardServings = $("<div>")
+        var cardCookingMinutes = $("<div>")
+        var cardImage = $("<img>")
+
+        // adding classes to match bootstrap
+        card.addClass("card")
+        cardBody.addClass("card-body")
+        cardImage.addClass("card-img-top")
+        cardTitle.addClass("card-title")
+        cardServings.addClass("card-text")
+        cardCookingMinutes.addClass("card-text")
+
+        // attr section
+        cardImage.attr("width", "18rem")
+        cardImage.attr("src", response.recipes[i].image)
+
+        //display text
+        cardTitle.text(response.recipes[i].title)
+        cardServings.text(response.recipes[i].servings)
+        cardCookingMinutes.text(response.recipes[i].readyInMinutes)
+
+        // append into card structure
+        $(".card-container").append(card)
+        card.append(cardImage)
+        card.append(cardBody)
+        cardBody.append(cardTitle)
+        cardBody.append(cardServings)
+        cardBody.append(cardCookingMinutes)
+
+
+
+
+        // $("#title-card").text(response.title)
+        // $("#servings-card").text(response.servings)
+        // $("#prep-time-card").text(response.cookingMinutes)
+        // $("#diet-type-card").text(response.diets[0])
+        // $("#recipe-img-card").text(response.images)
     }
 
     // function generateSingleRecipe (){}
