49164685858fd4016ba2b09c8ff652b9d1554431
David Garza <dgarza0413@gmail.com>
2019-10-05 15:37:36 -0500

    add auth for user sign in

--- client/src/components/Firebase/firebase.js
+++ client/src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
 const config = {
     apiKey: "AIzaSyAnXLn1HZYXp4nKnRRxuGiGYGx4vJx6FsA",
@@ -10,10 +11,45 @@ const config = {
     appId: "1:531404044060:web:2912dde85afc67ed"
 };
 
+// const prodConfig = {
+//     apiKey: process.env.REACT_APP_PROD_API_KEY,
+//     authDomain: process.env.REACT_APP_PROD_AUTH_DOMAIN,
+//     databaseURL: process.env.REACT_APP_PROD_DATABASE_URL,
+//     projectId: process.env.REACT_APP_PROD_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_PROD_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_PROD_MESSAGING_SENDER_ID,
+//   };
+//   const devConfig = {
+//     apiKey: process.env.REACT_APP_DEV_API_KEY,
+//     authDomain: process.env.REACT_APP_DEV_AUTH_DOMAIN,
+//     databaseURL: process.env.REACT_APP_DEV_DATABASE_URL,
+//     projectId: process.env.REACT_APP_DEV_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_DEV_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_DEV_MESSAGING_SENDER_ID,
+//   };
+//   const config =
+//     process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
+
 class Firebase {
     constructor() {
         app.initializeApp(config);
+
+        this.auth = app.auth();
     }
+    // **** AUTH API ****
+
+    doCreateUserWithEmailAndPassword = (email, password) =>
+        this.auth.createUserWithEmailAndPassword(email, password);
+
+    doSignInWithEmailAndPassword = (email, password) =>
+        this.auth.signInWithEmailAndPassword(email, password);
+
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+    doPasswordUpdate = password =>
+        this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
